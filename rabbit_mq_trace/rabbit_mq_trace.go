cf8cc12d6d326c517cd29387e8e567813e374fa9
Toufiq <toufiq.austcse@gmail.com>
2025-02-19 12:34:52 +0600

    feat: add rabbitmq publisher tracer

--- rabbit_mq_trace/rabbit_mq_trace.go
+++ rabbit_mq_trace/rabbit_mq_trace.go
@@ -8,18 +8,32 @@ import (
 	"go.opentelemetry.io/otel/trace"
 )
 
+const rabbitMqSpanName = "rabbitmq"
+
 func TraceRabbitMqConsumer(queueName string, consumerName string, ctx context.Context) (trace.Span, context.Context) {
 	spanName := "RabbitMq Consumer"
 	if consumerName != "" {
 		spanName = consumerName
 	}
-	msgCtx, span := otel.Tracer("rabbitmq").Start(ctx, spanName,
+	msgCtx, span := otel.Tracer(rabbitMqSpanName).Start(ctx, spanName,
 		trace.WithAttributes(
 			semconv.MessagingSystemKey.String("rabbitmq"),
 			semconv.MessagingOperationKey.String("receive"),
 			attribute.String("messaging.rabbitmq.queue", queueName),
 			//attribute.String("messaging.message_id", d.MessageId),
 			//attribute.Int("messaging.message_size", len(d.Body)),
+		),
+	)
+	return span, msgCtx
+}
+
+func TraceRabbitMqPublisher(exchange string, routingKey string, ctx context.Context) (trace.Span, context.Context) {
+	msgCtx, span := otel.Tracer(rabbitMqSpanName).Start(ctx, "AMQP Publish",
+		trace.WithAttributes(
+			semconv.MessagingSystemKey.String("rabbitmq"),
+			semconv.MessagingOperationKey.String("publish"),
+			attribute.String("messaging.rabbitmq.exchange", exchange),
+			attribute.String("messaging.rabbitmq.routing_key", routingKey),
 		),
 	)
 	return span, msgCtx
