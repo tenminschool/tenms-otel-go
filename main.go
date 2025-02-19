0d2f0d7e0f2af14cabeb7faa0eb157436c2b4c9a
Toufiq <toufiq.austcse@gmail.com>
2025-02-19 16:04:52 +0600

    refactor: refactor initialization

--- main.go
+++ main.go
@@ -8,34 +8,46 @@ import (
 	"github.com/tenminschool/tenms-otel-go/metrics"
 	"github.com/tenminschool/tenms-otel-go/tracer"
 	"go.opentelemetry.io/contrib/instrumentation/github.com/gin-gonic/gin/otelgin"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/propagation"
+	semconv "go.opentelemetry.io/otel/semconv/v1.10.0"
+	oteltrace "go.opentelemetry.io/otel/trace"
 	"gorm.io/gorm"
 	"gorm.io/plugin/opentelemetry/tracing"
 )
 
-type TenMsOtel struct {
-	tenMsOtelConfig *config.TenMsOtelConfig
+var tenMsOpenTelemetry *TenMsOtel
+
+func GetTenMsOtel() *TenMsOtel {
+	return tenMsOpenTelemetry
 }
 
-func NewTenMsOtel(serviceName string, insecureMode string, OtelExporterOtlpEndpoint string) *TenMsOtel {
-	return &TenMsOtel{tenMsOtelConfig: &config.TenMsOtelConfig{
+type TenMsOtel struct {
+	config *config.TenMsOtelConfig
+}
+
+func Boot(serviceName string, insecureMode string, OtelExporterOtlpEndpoint string) *TenMsOtel {
+	tenMsOpenTelemetry = &TenMsOtel{config: &config.TenMsOtelConfig{
 		ServiceName:              serviceName,
 		InsecureMode:             insecureMode,
 		OtelExporterOtlpEndpoint: OtelExporterOtlpEndpoint,
 	}}
+	return tenMsOpenTelemetry
 }
 
 func (tenmsOtel *TenMsOtel) Init(
 	Router *gin.Engine,
 	db *gorm.DB,
 ) func(ctx context.Context) {
-	shutDownTracer := tracer.InitTracer(tenmsOtel.tenMsOtelConfig)
+	shutDownTracer := tracer.InitTracer(tenmsOtel.config)
 
-	meterProvider := metrics.InitMeter(tenmsOtel.tenMsOtelConfig)
+	meterProvider := metrics.InitMeter(tenmsOtel.config)
 
-	meter := meterProvider.Meter(tenmsOtel.tenMsOtelConfig.ServiceName)
+	meter := meterProvider.Meter(tenmsOtel.config.ServiceName)
 	metrics.GenerateMetrics(meter)
 
-	RegisterMiddleware(Router, tenmsOtel.tenMsOtelConfig.ServiceName)
+	RegisterMiddleware(Router, tenmsOtel.config.ServiceName)
 
 	if db != nil {
 		if err := db.Use(tracing.NewPlugin()); err != nil {
@@ -45,9 +57,9 @@ func (tenmsOtel *TenMsOtel) Init(
 
 	fmt.Printf(
 		"Tenms otel initialized with service_name %s, insecure mode %s, OtelExporterOtlpEndpoint %s\n",
-		tenmsOtel.tenMsOtelConfig.ServiceName,
-		tenmsOtel.tenMsOtelConfig.InsecureMode,
-		tenmsOtel.tenMsOtelConfig.OtelExporterOtlpEndpoint,
+		tenmsOtel.config.ServiceName,
+		tenmsOtel.config.InsecureMode,
+		tenmsOtel.config.OtelExporterOtlpEndpoint,
 	)
 	return func(ctx context.Context) {
 		if err := shutDownTracer(ctx); err != nil {
@@ -60,6 +72,46 @@ func (tenmsOtel *TenMsOtel) Init(
 			fmt.Println("error in shut down meterProvider")
 		} else {
 			fmt.Println("meter provider shut down properly")
+		}
+	}
+}
+
+func (tenmsOtel *TenMsOtel) TraceMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		request := c.Request
+		ctx := request.Context()
+
+		// ref: https://github.com/open-telemetry/opentelemetry-go-contrib/blob/main/instrumentation/github.com/labstack/echo/otelecho/echo.go
+		ctx = otel.GetTextMapPropagator().Extract(ctx, propagation.HeaderCarrier(request.Header))
+		opts := []oteltrace.SpanStartOption{
+			oteltrace.WithAttributes(semconv.NetAttributesFromHTTPRequest("tcp", request)...),
+			oteltrace.WithAttributes(semconv.EndUserAttributesFromHTTPRequest(request)...),
+			oteltrace.WithAttributes(
+				semconv.HTTPServerAttributesFromHTTPRequest(tenmsOtel.config.ServiceName, c.FullPath(), request)...),
+			oteltrace.WithSpanKind(oteltrace.SpanKindServer),
+		}
+		spanName := c.FullPath()
+		if spanName == "" {
+			spanName = fmt.Sprintf("HTTP %s route not found", request.Method)
+		}
+
+		ctx, span := otel.Tracer("gin-http").Start(ctx, spanName, opts...)
+		defer span.End()
+
+		// pass the span through the request context
+		c.Request = request.WithContext(ctx)
+
+		c.Next()
+
+		// Record response details
+		span.SetAttributes(
+			attribute.Int("http.status_code", c.Writer.Status()),
+			attribute.Int("http.response_size", c.Writer.Size()),
+		)
+
+		// Record error if any
+		if len(c.Errors) > 0 {
+			span.RecordError(c.Errors.Last().Err)
 		}
 	}
 }
